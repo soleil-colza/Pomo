f773ba095185bdf53fb8a412b12d72be3dbc197c
Hina Nakahira <soleil.colza@icloud.com>
2024-02-16 18:15:02 +0900

    [add] focus timer

--- app/src/main/java/com/example/pomo/presentation/PomoActivity.kt
+++ app/src/main/java/com/example/pomo/presentation/PomoActivity.kt
@@ -2,8 +2,10 @@ package com.example.pomo.presentation
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,10 +20,20 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pomo.ui.theme.PomoTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class PomoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +44,7 @@ class PomoActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeScreen(onFabClick = {})
+                    FocusScreen()
                 }
             }
         }
@@ -68,14 +80,43 @@ fun HomeScreen(onFabClick: () -> Unit) {
                 Column(
                     modifier = Modifier.padding(16.dp)
                 ) {
-                    Text(text = "Log")
+                    Text(
+                        text = "Log",
+                        style = MaterialTheme.typography.headlineMedium)
                 }
         }
         }
     )
 }
 
+@Composable
+fun FocusScreen() {
 
+    var seconds by remember { mutableStateOf(0) }
+    var minutes by remember { mutableStateOf(24) }
+
+    LaunchedEffect(Unit) {
+        val timer = object : CountDownTimer(minutes * 60 * 1000L, 1000L) {
+            override fun onTick(millisUntilFinished: Long) {
+                seconds = (millisUntilFinished / 1000L).toInt() % 60
+            }
+
+            override fun onFinish() {
+                // タイマー終了時の処理
+            }
+        }
+        timer.start()
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "%02d:%02d".format(minutes, seconds),
+            fontSize = 50.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+
+}
 
 
 @Preview(showBackground = true)
