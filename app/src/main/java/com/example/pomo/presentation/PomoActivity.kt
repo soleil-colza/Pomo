c5c007e93740dc8fff256800ee2c6c370b80a179
Hina Nakahira <soleil.colza@icloud.com>
2024-02-16 15:56:08 +0900

    added PaddingValues to the top-level content of Scaffold

--- app/src/main/java/com/example/pomo/presentation/PomoActivity.kt
+++ app/src/main/java/com/example/pomo/presentation/PomoActivity.kt
@@ -1,11 +1,16 @@
 package com.example.pomo.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -33,19 +38,37 @@ class PomoActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun HomeScreen() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(text = "Log")
+fun HomeScreen(onFabClick: () -> Unit) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onFabClick,
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Add",
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        },
+        content = {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(text = "Log")
+                }
         }
-    }
+        }
+    )
 }
+
 
 
 
